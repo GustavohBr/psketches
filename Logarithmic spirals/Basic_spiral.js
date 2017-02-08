9a7a7c95cbfefeb94b656335ef9caa961628ad3d
Gustavo Braz <GustavohBr@users.noreply.github.com>
2017-02-08 15:38:12 -0200

    Added 2 sliders for 'a' and 'b' values

--- Logarithmic spirals/Basic_spiral.js
+++ Logarithmic spirals/Basic_spiral.js
@@ -1,17 +1,24 @@
-var e, a, b, t, p1, xt, yt;
+var e, a, b, t, p1, xt, yt, sliderA, sliderB;
 
 function setup() {
     createCanvas(400, 400);
     strokeWeight(2);
     stroke(0);
     e = exp(1.0); // Euler's number 'e'
-    a = 3;     //Change these values to modify 
-    b = 0.24;  //the shape of the spiral
+    a = 1;     //Change these values to modify 
+    b = 0.2;  //the shape of the spiral
     t = 0;
-
+    sliderA = createSlider(0, 100, 100);
+	  sliderA.position(50, 415);
+    sliderA.style('width', '100px');
+		sliderB = createSlider(0, 100, 100);
+	  sliderB.position(200, 415);
+    sliderB.style('width', '100px');
 }
 
 function draw() {
+		a = sliderA.value();
+		b = map(sliderB.value(), 0, 100, 0, 1);
     background(255);
     translate(width / 2, height / 2);
     noFill();
